bc27a6ec164bfab1b20af410a7397702aab4492d
leadot <leann.cartabona@gmail.com>
2016-10-15 21:25:03 +0200

    Create WordSort.rb

---
+++ WordSort.rb
@@ -0,0 +1,9 @@
+def new_order (sentence)
+  sentence['anymore.'] = 'anymore'
+  lowercase_sentence = sentence.downcase
+  b_sentence = lowercase_sentence.split
+  c_sentence = b_sentence.sort
+	print final_phrase = c_sentence
+end
+
+puts new_order("This sentence will no longer be a sentence anymore.")
